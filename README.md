5f7d4bd1a909de0b27de2d3e881f1c00ed944282
Casey Rock <rockc@udel.edu>
2020-02-26 05:36:18 -0800

    fisrt iteration of mycopy.c

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# systemCalls
+### systemCalls
+
+
+`open(filename, int flags, int mode)`
+
+open returns a int filedescriptor
+
+**open flags int constants**
+    1. O_RDONLY: read only 
+    2. O_WRONLY: write only
+    3. O_RDWR: read and write 
+    4. O_CREAT: create file if it doesn’t exist
+    5. O_EXCL: prevent creation if it already exists
+
+
+
+
+`write(fd, * buf, size_t count)`
+
+write() writes up to count bytes from the buffer starting at buf to
+       the file referred to by the file descriptor fd.
---
+++ mycopy.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
+
+
+void main(int argc, char *argv[]){
+
+    struct stat buf;
+    char override = ' ';
+    int file1Desc;
+    int file2Desc;
+    FILE *newfile;
+    size_t nbyts;
+
+
+    char file1Buf[20];
+
+
+
+    if(argc != 3){
+        perror("You must have two files too run mycopy.c");
+        exit(1);
+    }
+
+    if(stat(argv[1], &buf) < 0){
+        perror("File 1 does not exsit");
+        exit(1);
+    }
+
+    if(stat(argv[2], &buf) >= 0){
+        printf("Do you want to overide %s? (y/n) > ", argv[2]);
+        scanf("%c", &override);
+        file2Desc = open(argv[2], O_RDWR);
+    }else{
+        newfile = fopen(argv[2], "w");
+        file2Desc = open(argv[2], O_RDWR);
+    }
+    
+    
+
+    file1Desc = open(argv[1], O_RDWR);
+
+
+
+    read(file1Desc, file1Buf, nbyts);
+
+
+    if(override == 'y'){
+        write(file2Desc, file1Buf, 20);
+
+    }
+        
+
+    
+
+
+
+
+
+
+
+}
